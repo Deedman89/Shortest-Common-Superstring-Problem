880d89a6f97c2393f9dcfad4d1a73738400d2d86
Kirill Berkut <berkut.kiu@physech.edu>
2020-04-04 18:53:15 +0500

    upd Bonus.cpp

--- Bonus1_Berkut_Fomina/Bonus.cpp
+++ Bonus1_Berkut_Fomina/Bonus.cpp
@@ -1,9 +1,32 @@
 #include <iostream>
 #include <vector>
+#include <string>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+using namespace std;
+
+int Overlap(const string& s1, const string& s2)
+{
+    int overlap = 0;
+    int j = 0;
+    int s1_size = s1.size() - 1;
+    int s2_size = s2.size();
+    for (int i = s1_size; i > -1; i--)
+    {
+        j++;
+        for (j; j < s2_size; j++)
+        {
+            string prefix = s1.substr(i);
+            string sufix = s2.substr(0, j);
+            if (prefix == sufix)
+            {
+                overlap = j;
+                break;
+            }
+            else
+                break;
+        }
+    }
+    return overlap;
 }
 
 
