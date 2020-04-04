58a2bc55086bdf2aabbdce33e112e27bb203bc83
vvubbalubba <45140939+vvubbalubba@users.noreply.github.com>
2020-04-04 17:09:48 +0300

    Update Bonus.cpp

--- Bonus1_Berkut_Fomina/Bonus.cpp
+++ Bonus1_Berkut_Fomina/Bonus.cpp
@@ -4,7 +4,8 @@
 
 using namespace std;
 
-int Overlap(const string& s1, const string& s2)
+// Функция, считающая длину максимальной одинаковой подстроки данных на вход строк
+int Overlap(const string& s1, const string& s2)  
 {
     int overlap = 0;
     int j = 0;
@@ -29,4 +30,31 @@ int Overlap(const string& s1, const string& s2)
     return overlap;
 }
 
+
+class Graph { 
+public:
+    // Конструктор, создающий полный граф по набору строк input
+    Graph(const vector<string>& input){
+        int n = input.size();
+        size = n;
+        matrix.resize(n); // Создаем пустую матрицу смежности размера n*n
+        for (auto i : matrix) i.resize(n);
+        for (int i = 0; i < n; i++) // Заполняем матрицу смежности
+            for (int j = 0; j < n; j++) AddEdge(i, j, Overlap(input[i], input[j]));
+    };
+
+    // Добавление ребра из вершины from в вершину to с весом length
+    void AddEdge(int from, int to, int length){
+        matrix[from][to] = length;
+    };
+
+    // Получение списка ребер, исходящих из вершины v
+    vector<int> GetEdgesFrom(int v) const{
+        vector<int> res = matrix[v];
+    };
+
+private:
+    vector<vector<int>> matrix;
+    int size; // размер матрицы
+};
 
