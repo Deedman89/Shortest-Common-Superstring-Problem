8bb8213fa07f138112a2a53184e3aa59171a1a20
vvubbalubba <45140939+vvubbalubba@users.noreply.github.com>
2020-04-11 19:06:18 +0300

    Update Test.cpp

--- Bonus1_Berkut_Fomina/Test.cpp
+++ Bonus1_Berkut_Fomina/Test.cpp
@@ -1,1 +1,53 @@
 // Тестирующая программа
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cstdlib>
+#include <cstdint>
+#include <algorithm>
+#include <set>
+using namespace std;
+
+int main() {
+    int number_strings = 4;
+    //cin >> number_strings; // считаем кол-во строк
+    //vector<string> input = {"cde", "abc", "eab", "fgh", "ghf", "hed"};
+    vector<string> input = {"abba", "aca", "bac", "aa"};
+
+    // Протестируем функцию Overlap и создание графа
+    Graph graph(input);
+    //graph.Print();
+
+    // Протестируем функцию Assignment
+    vector<int> assign;
+    assign = Assignment(graph);
+    //for (auto i : assign) cout << i << " ";
+
+    // Протестируем функцию FullCoverage
+    vector<vector<int>> full_coverage;
+    full_coverage = FullCoverage(assign);
+    /*
+     * for (auto i : full_coverage) {
+        for (auto j : i) cout << j << " ";
+        cout << endl;
+       }
+     */
+
+    // Протестируем функцию
+    vector<int> cycl;
+    cycl = Minimize({1, 0, 2}, graph);
+    //for (auto i : cycl) cout << i << " ";
+
+    // Протестируем функцию Cycle
+    string s;
+    s = Cycle({2, 1, 0}, graph, input);
+    //cout << s;
+
+    // Протестируем Builder
+    string res;
+    res = Builder(full_coverage, graph, input);
+    cout << res;
+
+
+    return 0;
+}
